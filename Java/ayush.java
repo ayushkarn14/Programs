0f3233d0825fbbf14a8497910c15989365182fb3
Ayush Karn <ayushkarn14@gmail.com>
2022-07-30 13:33:54 +0530

    test2

--- Java/ayush.java
+++ Java/ayush.java
@@ -1,6 +1,6 @@
 class ayush {
     public static void main(String args[]) {
         Integer a = new Integer("abc");
-        // int arr[][] = { { 1, 2, 3 }, { 10 }, { 100, 200 } };
+        int arr[][] = { { 1, 2, 3 }, { 10 }, { 100, 200 } };
     }
 }
