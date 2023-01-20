4ea146be2f5154ec477372e0dafb271636fb6e99
ayushkarn14 <ayushkarn14@gmail.com>
2023-01-20 12:55:56 +0530

    yash ka dar

---
+++ C++/Algorithms/MergeSort.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void mergeSort(int *a, int f, int l)
+{
+    if (f > l)
+        return;
+    int mid = (f + l) / 2;
+    mergeSort(a, f, mid);
+    mergeSort(a, mid + 1, l);
+    merge(a, f, mid, l);
+}
+void merge(int a *, int f, ) int main()
+{
+}
