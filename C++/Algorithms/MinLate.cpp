30318f35eb901d748c1bde45c4a4261b436b355f
ayushkarn14 <ayushkarn14@gmail.com>
2023-05-09 02:49:39 +0530

    sample

---
+++ C++/Algorithms/MinLate.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+void minLateness(int n, vector<pair<int, pair<int, int>>> jobs)
+{//                                  d
+    sort(jobs.begin(), jobs.end());
+    map<int, pair<int, int>> sch;
+    int s = 0;
+    for (auto it : jobs)
+    {
+        sch[]
+    }
+}
+int main()
+{
+}
